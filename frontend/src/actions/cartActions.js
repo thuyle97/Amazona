0c96e9f48b4f44f62d667775ca1bbc43528cc123
Thuy Le <phuongthuy9796@gmail.com>
2021-11-15 22:49:39 -0800

    Implement Remove From Cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
   //get the data
@@ -17,5 +17,10 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
     },
   });
   //store items to local storage --> when refresh the page --> Not disappear
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (productId) => (dispatch, getState) => {
+  dispatch({ type: CART_REMOVE_ITEM, payload: productId });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
